cb4001d8a484e790a0bda16f3ee9229bc2d270a3
Minecraft-Space <minecraftspac3@gmail.com>
2016-10-29 03:10:32 +0200

    Added back button on GameScreen

--- core/src/com/nac/game/Screens/GameScreen.java
+++ core/src/com/nac/game/Screens/GameScreen.java
@@ -5,24 +5,61 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.nac.game.Driver;
 
 /**
  * Created by user on 10/29/2016.
  */
 public class GameScreen implements Screen {
+    Texture backInactive;
+    Texture backActive;
+    boolean back;
+    int x;
+    int y;
+
     Driver game;
-    Texture img = new Texture("cross.png");
     public GameScreen(Driver game) {
         this.game = game;
+        backInactive = new Texture("buttons/backInactive.png");
+        backActive = new Texture("buttons/backActive.png");
     }
 
     @Override
     public void render(float delta) {
         game.batch.begin();
         Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
-        game.batch.draw(img, 0, 0);
+        updateButtons();
+        renderButtons(game.batch);
+        if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+            buttonListener();
+        }
         game.batch.end();
+    }
+
+    private void buttonListener(){
+        if(back){
+            game.DisposeScreen();
+        }
+    }
+
+    private void renderButtons(SpriteBatch sb){
+        if(!back){
+            sb.draw(backInactive, Driver.width / 7 * 4, Driver.height / 10 * 8);
+        } else{
+            sb.draw(backActive, Driver.width / 7 * 4, Driver.height / 10 * 8);
+        }
+    }
+
+    private void updateButtons(){
+        x = Gdx.input.getX();
+        y = Driver.height - Gdx.input.getY();
+
+        if(x > Driver.width / 7 * 4 && x <  Driver.width / 7 * 4 + backInactive.getWidth() && y > Driver.height / 10 * 8 && y < Driver.height / 10 * 8 + backInactive.getHeight()){
+            back = true;
+        } else{
+            back = false;
+        }
     }
 
     //region rubbish
