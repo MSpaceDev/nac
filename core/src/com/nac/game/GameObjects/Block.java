2806b2065b41ea289323ee0adc35991d1874306d
Minecraft-Space <minecraftspac3@gmail.com>
2016-10-29 19:03:28 +0200

    Blocks render values

--- core/src/com/nac/game/GameObjects/Block.java
+++ core/src/com/nac/game/GameObjects/Block.java
@@ -1,31 +1,50 @@
 package com.nac.game.GameObjects;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.nac.game.Driver;
+import com.nac.game.Screens.GameScreen;
 
 /**
  * Created by HappySaila on 10/29/16.
  * individual square that is interactable
  */
-public class Block {
+public class Block{
     int val; //0=" " 1="X" 2="O"
-    Texture img;
+    Sprite cross;
+    Sprite naught;
     Board board;
     xY position;
     xY boardStart;
 
     public Block(int x, int y, Board board) {
+        val = 0;
         this.position = new xY(x, y);
         this.board = board;
         boardStart = board.boardStart;
 
         //instantiate textures
-        img = new Texture("cross.png");
+        cross = new Sprite(new Texture("cross.png"));
+        naught = new Sprite(new Texture("naught.png"));
+
+        //Set sizes
+        cross.setSize(board.blockSize, board.blockSize);
+        naught.setSize(board.blockSize, board.blockSize);
     }
 
     public void render(SpriteBatch sb){
-        sb.draw(img, boardStart.x + (position.x * board.blockSize), boardStart.y + (position.y * board.blockSize),
-                board.blockSize, board.blockSize);
+        renderIcons(sb);
+}
+
+    private void renderIcons(SpriteBatch sb){
+        if (val == 1){
+            cross.draw(sb);
+        } else if(val == 2){
+            naught.draw(sb);
+        }
     }
 
     public int getVal() {
