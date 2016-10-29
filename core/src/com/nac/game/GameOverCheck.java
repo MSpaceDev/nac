3fadafc1a73cba95e22de8151e27fb2b527a18c4
Grant Haldane Wilson <HappySaila@Grants-MacBook-Pro.local>
2016-10-29 02:34:33 +0200

    tree for checking game over state. game board is being created

---
+++ core/src/com/nac/game/GameOverCheck.java
@@ -0,0 +1,33 @@
+package com.nac.game;
+
+/**
+ * Created by HappySaila on 10/29/16.
+ * will check if a given board state is game over or not
+ */
+public class GameOverCheck {
+    public static boolean isGameOver(Board board){
+        Tree tree = generateTree(board);
+    }
+}
+
+class Node{
+    Node parent;
+    Node child1;
+    Node child2;
+    Node child3;
+
+    public Node(Node parent, Node child1, Node child2, Node child3) {
+        this.parent = parent;
+        this.child1 = child1;
+        this.child2 = child2;
+        this.child3 = child3;
+    }
+}
+
+class Tree{
+    Node root;
+
+    public Tree(Node root) {
+        this.root = root;
+    }
+}
